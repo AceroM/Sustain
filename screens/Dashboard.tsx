c91664b05502198b4f4713ef5db58a616066d38f
Miguel Acero <miguelacero528@gmail.com>
2021-09-18 18:39:37 -0500

    added carousel stylings

--- screens/Dashboard.tsx
+++ screens/Dashboard.tsx
@@ -3,10 +3,12 @@ import { LinearGradient } from "expo-linear-gradient";
 import rgba from "hex-to-rgba";
 import React, { Component } from "react";
 import {
-  Dimensions, FlatList, Image, Modal, ScrollView, StyleSheet, TouchableOpacity, View
+  Animated, Dimensions, FlatList, Image, ImageBackground, Modal, ScrollView, StyleSheet, TouchableOpacity, View
 } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 import * as Icon from "react-native-vector-icons";
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Octicons from 'react-native-vector-icons/Octicons';
 import { NavigationActions } from "react-navigation";
 import Badge from '../components/Badge';
 import Block, { styles as blockStyles } from '../components/Block';
@@ -14,7 +16,90 @@ import Button from '../components/Button';
 import Card, { styles as cardStyles } from '../components/Card';
 import Text from '../components/Text';
 import theme from "../constants/Green";
+import Layout from "../constants/Layout";
 const { width } = Dimensions.get("window");
+
+const mocks = [
+  {
+    id: 1,
+    user: {
+      name: 'Lelia Chavez',
+      avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
+    },
+    saved: true,
+    location: 'Santorini, Greece',
+    temperature: 34,
+    title: 'Santorini',
+    description: 'Santorini is one of the Cyclades islands in the Aegean Sea. It was devastated by a volcanic eruption in the 16th century BC, forever shaping its rugged landscape. The whitewashed, cubiform houses of its 2 principal towns, Fira and Oia, cling to cliffs above an underwater caldera (crater). They overlook the sea, small islands to the west and beaches made up of black, red and white lava pebbles.',
+    rating: 4.3,
+    reviews: 3212,
+    preview: 'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+    images: [
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+    ]
+  },
+  {
+    id: 2,
+    user: {
+      name: 'Lelia Chavez',
+      avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
+    },
+    saved: false,
+    location: 'Loutraki, Greece',
+    temperature: 34,
+    title: 'Loutraki',
+    description: 'This attractive small town, 80 kilometers from Athens',
+    rating: 4.6,
+    reviews: 3212,
+    preview: 'https://images.unsplash.com/photo-1458906931852-47d88574a008?auto=format&fit=crop&w=800&q=80',
+    images: [
+      'https://images.unsplash.com/photo-1458906931852-47d88574a008?auto=format&fit=crop&w=800&q=80',
+      'https://images.unsplash.com/photo-1446903572544-8888a0e60687?auto=format&fit=crop&w=800&q=80',
+    ]
+  },
+  {
+    id: 3,
+    user: {
+      name: 'Lelia Chavez',
+      avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
+    },
+    saved: true,
+    location: 'Santorini, Greece',
+    temperature: 34,
+    title: 'Santorini',
+    description: 'Santorini - Description',
+    rating: 3.2,
+    reviews: 3212,
+    preview: 'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+    images: [
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+      'https://images.unsplash.com/photo-1507501336603-6e31db2be093?auto=format&fit=crop&w=800&q=80',
+    ]
+  },
+  {
+    id: 4,
+    user: {
+      name: 'Lelia Chavez',
+      avatar: 'https://randomuser.me/api/portraits/women/44.jpg',
+    },
+    location: 'Loutraki, Greece',
+    temperature: 34,
+    title: 'Loutraki',
+    description: 'This attractive small town, 80 kilometers from Athens',
+    rating: 5,
+    reviews: 3212,
+    preview: 'https://images.unsplash.com/photo-1458906931852-47d88574a008?auto=format&fit=crop&w=800&q=80',
+    images: [
+      'https://images.unsplash.com/photo-1458906931852-47d88574a008?auto=format&fit=crop&w=800&q=80',
+      'https://images.unsplash.com/photo-1446903572544-8888a0e60687?auto=format&fit=crop&w=800&q=80',
+    ]
+  },
+]
 
 const myCharities = [
   {
@@ -105,6 +190,7 @@ const profile = {
 };
 
 class Dashboard extends Component {
+  scrollX = new Animated.Value(0);
   state = {
     showModal: false,
     showCC: false,
@@ -321,6 +407,55 @@ class Dashboard extends Component {
     );
   }
 
+  renderDestination = item => {
+    const { navigation } = this.props;
+
+    return (
+      <TouchableOpacity style={styles.dCard} activeOpacity={0.8} onPress={() => navigation.navigate('Article', { article: item })}>
+        <ImageBackground
+          style={[styles.flex, styles.destination, styles.shadow]}
+          imageStyle={{ borderRadius: theme.sizes.radius }}
+          source={{ uri: item.preview }}
+        >
+          <View style={[styles.row, { justifyContent: 'space-between' }]}>
+            <View style={{ flex: 0 }}>
+              <Image source={{ uri: item.user.avatar }} style={styles.avatar} />
+            </View>
+            <View style={[styles.column, { flex: 2, paddingHorizontal: theme.sizes.padding / 2 }]}>
+              <Text style={{ color: theme.colors.white, fontWeight: 'bold' }}>{item.user.name}</Text>
+              <Text style={{ color: theme.colors.white }}>
+                <Octicons
+                  name="location"
+                  size={theme.sizes.font * 0.8}
+                  color={theme.colors.white}
+                />
+                <Text style={{ color: 'white' }}> {item.location}</Text>
+              </Text>
+            </View>
+            <View style={{ flex: 0, justifyContent: 'center', alignItems: 'flex-end', }}>
+              <Text style={styles.rating}>{item.rating}</Text>
+            </View>
+          </View>
+        </ImageBackground>
+        <View style={[styles.column, styles.destinationInfo, styles.shadow]}>
+          <Text style={{ fontSize: theme.sizes.font * 1.25, fontWeight: '500', paddingBottom: 8, }}>
+            {item.title}
+          </Text>
+          <View style={[styles.row, { justifyContent: 'space-between', alignItems: 'flex-end', }]}>
+            <Text style={{ color: theme.colors.caption }}>
+              {item.description.split('').slice(0, 50)}...
+            </Text>
+            <FontAwesome
+              name="chevron-right"
+              size={theme.sizes.font * 0.75}
+              color={theme.colors.caption}
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
   render() {
     const { profile, navigation, charities } = this.props;
     console.log("updated", this.props.charities);
@@ -496,6 +631,23 @@ class Dashboard extends Component {
           </Block>
         </ScrollView>
         {this.renderDonationMessage()}
+        <View style={[styles.column, styles.destinations]}>
+          <FlatList
+            horizontal
+            pagingEnabled
+            scrollEnabled
+            showsHorizontalScrollIndicator={false}
+            decelerationRate={0}
+            scrollEventThrottle={16}
+            snapToAlignment="center"
+            style={{ overflow: 'visible', height: 280 }}
+            data={mocks}
+            keyExtractor={(item, index) => `${item.id}`}
+            onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: this.scrollX } } }])}
+            renderItem={({ item }) => this.renderDestination(item)}
+          />
+          {/* {this.renderDots()} */}
+        </View>
       </ScrollView>
     );
   }
@@ -549,4 +701,116 @@ const styles = StyleSheet.create({
     right: theme.sizes.base,
     top: theme.sizes.base,
   },
+  destinations: {
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingBottom: 30,
+  },
+  flex: {
+    flex: 0,
+  },
+  column: {
+    flexDirection: 'column'
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  header: {
+    backgroundColor: theme.colors.white,
+    paddingHorizontal: theme.sizes.padding,
+    paddingTop: theme.sizes.padding * 1.33,
+    paddingBottom: theme.sizes.padding * 0.66,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  dCard: {
+    marginLeft: Layout.spacing * 2,
+  },
+  destination: {
+    width: width - (theme.sizes.padding * 2),
+    height: width * 0.6,
+    marginHorizontal: theme.sizes.margin,
+    paddingHorizontal: theme.sizes.padding,
+    paddingVertical: theme.sizes.padding * 0.66,
+    borderRadius: theme.sizes.radius,
+  },
+  destinationInfo: {
+    position: 'absolute',
+    borderRadius: theme.sizes.radius,
+    paddingHorizontal: theme.sizes.padding,
+    paddingVertical: theme.sizes.padding / 2,
+    bottom: 20,
+    left: 40,
+    backgroundColor: theme.colors.white,
+    width: width - (theme.sizes.padding * 3),
+  },
+  recommended: {
+  },
+  recommendedHeader: {
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    paddingHorizontal: theme.sizes.padding,
+  },
+  recommendedList: {
+  },
+  recommendation: {
+    width: (width - (theme.sizes.padding * 2)) / 2,
+    marginHorizontal: 8,
+    backgroundColor: theme.colors.white,
+    overflow: 'hidden',
+    borderRadius: theme.sizes.radius,
+    marginVertical: theme.sizes.margin * 0.5,
+  },
+  recommendationHeader: {
+    overflow: 'hidden',
+    borderTopRightRadius: theme.sizes.radius,
+    borderTopLeftRadius: theme.sizes.radius,
+  },
+  recommendationOptions: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: theme.sizes.padding / 2,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  recommendationTemp: {
+    fontSize: theme.sizes.font * 1.25,
+    color: theme.colors.white
+  },
+  recommendationImage: {
+    width: (width - (theme.sizes.padding * 2)) / 2,
+    height: (width - (theme.sizes.padding * 2)) / 2,
+  },
+  rating: {
+    fontSize: theme.sizes.font * 2,
+    color: theme.colors.white,
+    fontWeight: 'bold'
+  },
+  shadow: {
+    shadowColor: theme.colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 18,
+    elevation: 5,
+  },
+  dots: {
+    width: 10,
+    height: 10,
+    borderWidth: 2.5,
+    borderRadius: 5,
+    marginHorizontal: 6,
+    backgroundColor: theme.colors.gray,
+    borderColor: 'transparent',
+  },
+  activeDot: {
+    width: 12.5,
+    height: 12.5,
+    borderRadius: 6.25,
+    borderColor: theme.colors.active,
+  }
 });
